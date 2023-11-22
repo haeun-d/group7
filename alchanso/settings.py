5fe7d477045b111cee4fc7ef383e8ea7ee495bc5
jiwon <yjwon6621@duksung.ac.kr>
2023-11-22 10:36:12 +0900

    feat: community 메인페이지, post list 페이지 구현

--- alchanso/settings.py
+++ alchanso/settings.py
@@ -46,7 +46,6 @@ INSTALLED_APPS = [
     'users',
     'main',
     'consumption',
-
     'widget_tweaks',
 ]
 
--- community/urls.py
+++ community/urls.py
@@ -1,8 +1,19 @@
 from django.urls import path
+from . import views
 from .views import *
 
-app_name='community'
+app_name = 'community'
 
-urlpatterns=[
-    
+urlpatterns = [
+    #path('', views.PostList.as_view()),
+    #path('<int:pk>/', views.PostDetail.as_view()),
+    path('community-main/',community_main, name="community-main"),
+    path('food/',food,name='food'),
+    path('delivery/', delivery, name='delivery'),
+    path('food-post/',food_post,name='food-post'),
+    path('delivery-post/',delivery_post,name='delivery-post'),
+    path('create-post/',create_post,name='create-post'),
+    path('search/',search,name='search'),
+
 ]
+
