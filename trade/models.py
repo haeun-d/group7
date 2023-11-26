833dc302f2d06515ad648c2a8250ba5f430b0f94
noeyadd <wkdekdus6778@naver.com>
2023-11-26 12:33:16 +0900

    feat:삭제하기 버튼 만들기

--- trade/models.py
+++ trade/models.py
@@ -1,6 +1,7 @@
 from django.db import models
+from users.models import User
 
-# Create your models here.
+
 class Post(models.Model):
     title = models.CharField(max_length=50)
     body = models.TextField()
@@ -9,6 +10,8 @@ class Post(models.Model):
     place = models.CharField(max_length=100)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
+
 
     def __str__(self):
         return f'[{self.pk}]{self.title}'
