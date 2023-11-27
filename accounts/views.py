945e814f20080751a4561189e889401f6f3b9399
7beunseo <kimes0403@gmail.com>
2023-11-27 13:25:51 +0900

    feat: 마이페지에 내가 요청한 채팅 추가, 내가 올린 중고상품 열린 채팅조회 추가

--- accounts/views.py
+++ accounts/views.py
@@ -1,8 +1,8 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.forms import AuthenticationForm
 from django.contrib.auth import login, logout
 from .forms import  SignUpForm
-
+from trade.models import *
 # 회원가입
 def signup_view(request):
     if request.method == "GET":
@@ -17,23 +17,24 @@ def signup_view(request):
     else:
         return render(request, 'accounts/signup.html', {'form': form})
 
-
+# 로그인
 def login_view(request):
     if request.method=="GET":
         return render(request, 'accounts/login.html', {'form':AuthenticationForm})
-
     form=AuthenticationForm(request, data=request.POST)
     if form.is_valid():
         login(request, form.user_cache)
         return redirect('main')
     return render(request, 'accounts/login.html',{'form':form})
 
+# 로그아웃
 def logout_view(request):
     if request.user.is_authenticated:
         logout(request)
 
     return redirect('main')
 
+# 마이페이지
 def mypage(request):
     return render(request, 'accounts/mypage.html')
 
@@ -64,3 +65,32 @@ def map_scrap(request):
         'maps':maps
     }
     return render(request, 'accounts/map_scrap.html', context)
+
+# 내가 올린 중고거래
+def my_trade(request):
+    trades=Post.objects.filter(author=request.user)
+    context={
+        'trades':trades
+    }
+    return render(request, 'accounts/my_trade.html',context)
+
+def my_trade_detail(request, post_id):
+    post=get_object_or_404(Post, id=post_id)
+    context={
+        'post':post
+    }
+    return render(request, 'accounts/trade_detail.html',context)
+
+# 해당하는 중고거래에서 생긴 채팅방 조회
+def seller_chat(request, post_id):
+    chat_room=ChatRoom.objects.filter(post=post_id, seller=request.user)
+    return render(request, 'accounts/seller_chat.html',{'chat_room':chat_room})
+
+def proceeding_chat(request):
+    chat_room=ChatRoom.objects.filter( buyer=request.user)
+    return render(request,'accounts/buyer_chat.html',{'chat_room':chat_room} )
+
+def leave_chat_room(request, room_id):
+    chat_room=ChatRoom.objects.filter(id=room_id)
+    chat_room.delete()
+    return redirect('accounts:proceeding-chat')
--- trade/views.py
+++ trade/views.py
@@ -57,15 +57,15 @@ def chat(request, pk):
     return redirect('trade:chating',pk, chat_room.id)
 
 
-def chat(request, pk):
-    post = get_object_or_404(Post, pk=pk)
+def chat(request, post_id):
+    post=get_object_or_404(Post, pk=post_id)
     # 처음 열었을 경우는 저장, 아니면 그냥 값 들고옴 
     chat_room, created = ChatRoom.objects.get_or_create(
         buyer=request.user,
         seller=post.author,
         post=post
     )
-    return redirect('trade:chating', pk, chat_room.id)
+    return redirect('trade:chating', post_id, chat_room.id)
 
 def into_chatroom(request, post_id, chatroom_id):
     chat_room=get_object_or_404(ChatRoom, id=chatroom_id)
@@ -103,4 +103,3 @@ def liked_posts(request):
     # 현재 로그인한 사용자가 찜한 글들을 가져옴
     liked_posts = request.user.liked_posts.all()
     return render(request, 'trade/liked_posts.html', {'liked_posts': liked_posts})
-
