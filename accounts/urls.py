475cf5e86b13ecc43c77a7e6aca5a41cc3674eab
7beunseo <kimes0403@gmail.com>
2023-11-30 15:36:15 +0900

    feat: 마이페이에서 내가 쓴 글만 조회, 판매완료 필터링

--- accounts/urls.py
+++ accounts/urls.py
@@ -16,5 +16,7 @@ urlpatterns=[
     path('chatroom/<int:post_id>/',seller_chat, name='seller-chat'),
     path('proceeding-chat',proceeding_chat, name='proceeding-chat'),
     path('leave-chat-room/<int:room_id>/',leave_chat_room, name='leave-chat-room'),
-    path('review-list/',review_make, name='review-list')
+    path('review-list/',review_make, name='review-list'),
+    path('my-post-food/',my_post_food, name='my-post-food'),
+    path('my-post-ott/',my_post_ott, name='my-post-ott'),
 ]
