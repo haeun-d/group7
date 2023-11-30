81dfab1885a0e76c46633c6a0fd3856444a1904c
7beunseo <kimes0403@gmail.com>
2023-11-30 10:59:48 +0900

    fix: 커뮤니티 author exception 에러 해결

--- community/forms.py
+++ community/forms.py
@@ -14,7 +14,7 @@ class CommentDeliveryForm(forms.ModelForm):
 class PostForm(forms.ModelForm):
     class Meta:
         model=Post
-        fields=['title', 'content','head_image','file_upload','category']
+        fields=['title', 'content','head_image','file_upload','category','author']
 
     def __init__(self, *args, **kwargs):
         # Get the user from the form's kwargs
